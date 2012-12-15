7f94166a171a73e247e6a10caba47aa9788e74e9
dvilchez <dvilchezm@gmail.com>
2012-12-15 16:30:43 +0100

    lexer

---
+++ clisp.test/clisp.test/CLisp.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Machine.Specifications;
+using clisp.test.lexer;
+
+
+namespace clisp.test.clips
+{
+    [Subject("EvaluateExpression")]
+    public class when_atom_is_data
+    {
+        private Establish context = () =>
+        {
+            _exp = "5 \"abc\"";
+            _clisp = new CLispMachine();
+        };
+        private Because of = () => { _ret = _clisp.Evaluate(null, _exp); };
+        private It should_return_itself = () => _ret.ShouldEqual("5\n\"abc\"");
+
+        private static object _ret;
+        private static string _exp;
+        private static CLispMachine _clisp;
+    }
+
+    [Subject("EvaluateExpression")]
+    public class when_atom_is_an_undefined_variable
+    {
+        private Establish context = () =>
+        {
+            _exp = "abc";
+            _clisp = new CLispMachine();
+        };
+        private Because of = () =>
+        {
+            _exception = Catch.Exception(() => _clisp.Evaluate(new Dictionary<string, string>(), _exp));
+        };
+
+        private It should_return_a_exception = () => _exception.ShouldBeOfType<Exception>();
+
+        private static string _exp;
+        private static CLispMachine _clisp;
+        private static Exception _exception;
+    }
+
+    [Subject("EvaluateExpression")]
+    public class when_atom_is_a_defined_variable
+    {
+        private Establish context = () =>
+        {
+            _exp = "abc";
+            _clisp = new CLispMachine();
+        };
+        private Because of = () =>
+        {
+            _ret = _clisp.Evaluate(new Dictionary<string, string> { { "abc", "5" } }, _exp);
+        };
+
+        private It should_return_value = () => _ret.ShouldEqual("5");
+
+        private static string _exp;
+        private static CLispMachine _clisp;
+        private static string _ret;
+    }
+
+    [Subject("EvaluateExpression")]
+    public class when_atom_is_nil
+    {
+        private Establish context = () =>
+        {
+            _exp = "NIL";
+            _clisp = new CLispMachine();
+        };
+        private Because of = () =>
+        {
+            _ret = _clisp.Evaluate(new Dictionary<string, string>(), _exp);
+        };
+
+        private It should_return_value = () => _ret.ShouldEqual("NIL");
+
+        private static string _exp;
+        private static CLispMachine _clisp;
+        private static string _ret;
+    }
+
+    [Subject("EvaluateExpression")]
+    public class when_atom_is_T
+    {
+        private Establish context = () =>
+        {
+            _exp = "T";
+            _clisp = new CLispMachine();
+        };
+        private Because of = () =>
+        {
+            _ret = _clisp.Evaluate(new Dictionary<string, string>(), _exp);
+        };
+
+        private It should_return_value = () => _ret.ShouldEqual("T");
+
+        private static string _exp;
+        private static CLispMachine _clisp;
+        private static string _ret;
+    }
+
+
+    internal class EnvironmentMock
+    {
+    }
+
+    [Subject("EvaluateExpression")]
+    public class when_empty_list
+    {
+        private Establish context = () =>
+        {
+            _exp = "()";
+            _clisp = new CLispMachine();
+        };
+        private Because of = () =>
+        {
+            _ret = _clisp.Evaluate(new Dictionary<string, string>(), _exp);
+        };
+
+        private It should_return_nil = () => _ret.ShouldEqual("NIL");
+
+        private static object _ret;
+        private static string _exp;
+        private static CLispMachine _clisp;
+    }
+
+
+    public class CLispMachine
+    {
+        public string Evaluate(Dictionary<string, string> memory, string exp)
+        {
+            var result = "";
+            var tokens = new Lexer().Tokenizer(exp);
+            if (tokens.Any() && tokens.First() == "(")
+                return "NIL";
+
+            foreach (var token in tokens)
+            {
+                if (Regex.IsMatch(token, @"[\d]+") || Regex.IsMatch(token, "\\\".+\\\""))
+                {
+                    result += token + "\n";
+                }
+                else
+                {
+                    switch (token)
+                    {
+                        case "NIL":
+                            return "NIL";
+                        case "T":
+                            return "T";
+                        default:
+                            break;
+                    }
+                    if (memory.ContainsKey(token))
+                        return memory[token];
+                    throw new Exception();
+                }
+            }
+
+            return result.TrimEnd('\n');
+        }
+    }
+}
---
+++ clisp.test/clisp.test/Lexer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Machine.Specifications;
+
+namespace clisp.test.lexer
+{
+    [Subject("Tokenizer")]
+    public class when_empty_list
+    {
+        private Establish context = () =>
+                                        {
+                                            _exp = "()";
+                                            _lexer=new Lexer();
+                                        };
+        private Because of = () => _ret = _lexer.Tokenizer(_exp);
+        private It should_return_two_tokens = () => _ret.Count().ShouldEqual(2);
+        private It first_should_be_open_parenthesis = () => _ret.First().ShouldEqual("(");
+        private It last_should_be_close_parenthesis = () => _ret.Last().ShouldEqual(")");
+        private static IEnumerable<string> _ret;
+        private static Lexer _lexer;
+        private static string _exp;
+    }
+
+    [Subject("Tokenizer")]
+    public class when_list_with_forms
+    {
+        private Establish context = () =>
+        {
+            _exp = "(1 2 3)";
+            _lexer = new Lexer();
+        };
+        private Because of = () => _ret = _lexer.Tokenizer(_exp);
+        private It should_return_two_plus_number_of_forms = () => _ret.Count().ShouldEqual(5);
+        private It first_should_be_open_parenthesis = () => _ret.First().ShouldEqual("(");
+        private It last_should_be_close_parenthesis = () => _ret.Last().ShouldEqual(")");
+        private static IEnumerable<string> _ret;
+        private static Lexer _lexer;
+        private static string _exp;
+    }
+
+    [Subject("Tokenizer")]
+    public class when_complex_list
+    {
+        private Establish context = () =>
+        {
+            _exp = "(defun doble (x) (* x 25))";
+            _lexer = new Lexer();
+        };
+        private Because of = () => _ret = _lexer.Tokenizer(_exp);
+        private It should_return_two_plus_number_of_forms = () => _ret.Count().ShouldEqual(12);
+        private It second_should_be_defun = () => _ret.Skip(1).First().ShouldEqual("defun");
+        private It eighth_should_be_star = () => _ret.Skip(7).First().ShouldEqual("*");
+        private static IEnumerable<string> _ret;
+        private static Lexer _lexer;
+        private static string _exp;
+    }
+
+    public class Lexer
+    {
+        public IEnumerable<string> Tokenizer(string exp)
+        {
+            var parts = Regex.Matches(exp, @"[\(\)]|[^\(\)\s]+");
+
+            return from Match part in parts select part.Value;
+        }
+    }
+
+}
